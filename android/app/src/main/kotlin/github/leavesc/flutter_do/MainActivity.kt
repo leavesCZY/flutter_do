4f2943df209fe37b4b7a9a45cdfa0b4d1232411d
leavesC <1990724437@qq.com>
2021-03-15 17:42:27 +0800

    update

--- android/app/src/main/kotlin/github/leavesc/flutter_do/MainActivity.kt
+++ android/app/src/main/kotlin/github/leavesc/flutter_do/MainActivity.kt
@@ -1,18 +1,6 @@
 package github.leavesc.flutter_do
 
-import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
 
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
-
-/**
- * @Author: leavesC
- * @Date: 2020/11/4 16:35
- * @Desc:
- */
 class MainActivity : FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        GeneratedPluginRegistrant.registerWith(this)
-    }
 }
